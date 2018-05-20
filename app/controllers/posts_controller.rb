c659da1b71565ce2bb4661cb855bb6a249fa8bfc
bk8335 <bk8335@gmail.com>
2018-05-20 21:42:08 +0100

    set environment variables

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+	before_action :authenticate, only: [:admin, :new, :create, :edit, :update, :destroy]
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
 
 	def index
@@ -44,6 +45,19 @@ class PostsController < ApplicationController
 		@post.destroy
 		flash[:success] = 'The post was deleted'
 		redirect_to root_path
+	end
+
+	def admin
+		redirect_to root_path if authenticate
+	end
+
+protected
+	def authenticate
+		authenticate_or_request_with_http_basic do |username, password|
+			admin_username = Rails.application.secrets.admin_username
+   		admin_password = Rails.application.secrets.admin_password
+   		username == admin_username && password == admin_password 
+		end
 	end
 
 private
