1db2bec3d0e82718a316d910144aeb714df2b901
bk8335 <bk8335@gmail.com>
2018-06-08 22:52:13 +0100

    showing positions info from table in portfolio page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,7 @@ Category.create(name: 'Investing')
 Category.create(name: 'Trading')
 Category.create(name: 'Personal Finance')
 Category.create(name: 'Reading')
+
+Post.create(title: 'First blog post', content: 'blabla', category_id: Category.first.id, author: 'bla', draft: false)
+
+Position.create(asset: 'Tesla',	ticker: 'TSLA', long_or_short: 'short', initial_price: 317.76, post_id: Post.first.id)
