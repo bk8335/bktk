99f75a064886eb585affd265843b5651589bbdea
bk8335 <bk8335@gmail.com>
2018-06-14 11:01:57 +0100

    moved logic out of the view

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,22 @@
 class Position < ApplicationRecord
 
 	belongs_to :post
-	validates :asset, :long_or_short, :initial_price, :post_id, presence: true
+	validates :asset, :long_or_short, :initial_price, :post_id, :currency, presence: true
+
+	def position_return(latest_price, initial_price)
+		if long_or_short == 'short'
+			(((latest_price / initial_price) - 1) * 100).round(2) * -1
+		else
+			(((latest_price / initial_price) - 1) * 100).round(2)
+		end 
+	end
+
+	def get_latest_price(ticker)
+		if ticker.nil?
+			return "N/A"
+		else
+		((JSON.parse(open("https://api.iextrading.com/1.0/stock/#{ticker}/quote").read)['latestPrice']))
+		end
+	end
 
 end
